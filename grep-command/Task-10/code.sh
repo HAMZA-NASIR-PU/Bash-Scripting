5e9b0e9b09532d6aa5b121f4c828fac7664729b1
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-01-16 10:40:12 +0500

    grep command - Task 10 new solution

--- grep-command/Task-10/code.sh
+++ grep-command/Task-10/code.sh
@@ -9,6 +9,8 @@ grep -P 'Failed password for (?!root\b|daemon\b|syslog\b)[a-zA-Z][a-zA-Z0-9_-]* 
 
 grep -P 'Failed password for (?!root\b|daemon\b|syslog\b)[A-za-z0-9_-]+' app.log
 
+grep -P 'Failed password for (?!root\b|daemon\b|syslog)' app.log
+
 # The general format is X(?!Y), which matches "X" only if it is not immediately followed by "Y". 
 
 # A lookahead does NOT consume text, it only checks what comes next at that exact position. 
