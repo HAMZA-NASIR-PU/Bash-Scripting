38c47a4e4f4b7debf7e3d0acdd2d6780d243e150
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-03-05 13:24:27 +0500

    Bash scripts - Various Modifications

--- scripts/Task-11/code.sh
+++ scripts/Task-11/code.sh
@@ -8,13 +8,13 @@ func2() {
     local count2=52
 }
 
-echo "Before calling func: $count"
+echo "Before calling func: $count" # Nothing
 func
-echo "After calling func: $count"
+echo "After calling func: $count" # 22
 
 echo -e "\n\n"
 
-echo "Before calling func2: $count2"
+echo "Before calling func2: $count2" # Nothing
 func2
-echo "After calling func2: $count2"
+echo "After calling func2: $count2" # Nothing
 
