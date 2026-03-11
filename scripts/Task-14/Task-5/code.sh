12e689627c4b054a2093d32ba222c244f7065435
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-03-11 09:37:02 +0500

    Bash script - Task-5  of Task-14

---
+++ scripts/Task-14/Task-5/code.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+read -p "Enter password: " password
+
+CHECK="true"
+
+if ((${#password} < 8)); then
+    echo "Password must be at least 8 characters."
+    CHECK="false"
+fi
+
+if [[ ! $password =~ [[:digit:]] ]]; then
+    echo "Password must contain a number."
+    CHECK="false"
+fi
+
+if [[ $password =~ [[:space:]] ]]; then
+    echo "Password must not contain spaces."
+    CHECK="false"
+fi
+
+if [[ ! $password =~ [[:upper:]] ]]; then
+    echo "Password must contain an Uppercase letter."
+    CHECK="false"
+fi
+
+if [[ $CHECK == "true" ]]; then
+    echo "Strong password."
+fi
+
