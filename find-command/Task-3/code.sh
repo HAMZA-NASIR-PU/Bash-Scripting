8f04cdf12a7c7b0d001e340e31fef3907bdca65c
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-06-08 07:02:47 +0500

    find-command - Complete Task 3

---
+++ find-command/Task-3/code.sh
@@ -0,0 +1,20 @@
+
+# Understand the use of -print0 in find command and -0 flag in xargs command.
+
+mkdir -p lab
+cd lab
+
+touch "UserDTO.java"
+touch "Order DTO.java"
+touch "Customer
+DTO.java"
+touch "PaymentDTO.java"
+
+echo "DTO" > "UserDTO.java"
+echo "DTO" > "Order DTO.java"
+echo "DTO" > "Customer
+DTO.java"
+echo "DTO" > "PaymentDTO.java"
+
+
+
