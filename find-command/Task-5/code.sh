1086c5a12375b8228e9946faf830d810a1fe17fc
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-06-13 19:29:26 +0500

    find-command - Modification in Task-5

--- find-command/Task-5/code.sh
+++ find-command/Task-5/code.sh
@@ -3,14 +3,23 @@
 # This will not prune resources folder.
 find . \( -type d -name "*resources*" -o -name "*bin*" -prune \) -o -print
 
+# This command only prints bin folder
+find . \( -type d -name "*resources*" -o -name "*bin*" -exec echo "{}" \; \)
+# Output:
+# ./bin
+
+# But the following command will print all those folders/files that have bin in their path.
+find . -path "*bin*"
+
+
 # This will show the proof.
 grep 'resources' <(find . \( -type d -name "*resources*" -o -name "*bin*" -prune \) -o -print)
 
 # This will prune both resources and bin folder
-find . \( -type d \( -name "*resources*" -o -name "*school*" \) -prune \) -o -print
+find . \( -type d \( -name "*resources*" -o -name "*bin*" \) -prune \) -o -print
 
 # Better version of above command:
-find . \( -type d \( -name "resources" -o -name "school" \) -prune \) -o -print
+find . \( -type d \( -name "resources" -o -name "bin" \) -prune \) -o -print
 
 
 # (
@@ -19,7 +28,7 @@ find . \( -type d \( -name "resources" -o -name "school" \) -prune \) -o -print
 #     (
 #         name matches *resources*
 #         OR
-#         name matches *school*
+#         name matches *bin*
 #     )
 #     AND
 #     prune
@@ -29,4 +38,14 @@ find . \( -type d \( -name "resources" -o -name "school" \) -prune \) -o -print
 
 
 find .   \( -type d \( -name "*resources*" -o -name "*bin*" -o -name "*git*" -o -name "*target*" \) \
- -exec echo "PRUNED: {}" \; -prune \)   -o -print
+ -exec echo "PRUNED: {}" \; -prune \) -o -print
+
+find . \( -type d \( -name "*resources*" -o -name "*bin*" -o -name "*git*" \
+ -o -name "*target*" -o -name "*test*" -o -name "*database-backup*" \) \) \
+  \( -exec echo "PRUNED: {}" \; -prune \) -o -print
+
+
+# This command will just print ./bin and ./src/main/resources and nothing else.
+# So, its better to use -o -print at the end to print other stuff.
+find . \( -type d \( -name "*resources*" -o -name "*bin*" \) \) -prune
+
