a30800876063d5c03192471871a16c94b94c549c
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-06-17 16:14:34 +0500

    find-command - Complete Task-7

---
+++ find-command/Task-7/code.sh
@@ -0,0 +1,16 @@
+
+# Suppose you want to send the project structure to the chatgpt
+
+find ./src/main/java/com/example/demo -type f
+
+# To copy all the dtos that strat with the word 'event' -> Use -iname for case-insensitive.
+
+find ./src/main/java/com/example/demo/dto/ -iname "event*"
+
+# To copy all the code of java to a clipboard so that you can paste them easily in chatgpt.
+find ./src/main/java/com/example/demo/dto/ -iname "Event*" -type f -print0 | while IFS= read -r -d '' file; do     echo "===== $file =====";     cat "$file";     echo; done | clip
+
+
+find ./src/main/java/com/school/portal/dto/ -iname "Event*" -type f -print0 | tr '\0' '\n'
+
+
