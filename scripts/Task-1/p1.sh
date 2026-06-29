3a650aef6ab057632acf6c2725604157e36654f4
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2026-06-29 17:41:10 +0500

    Bash scripts - Modification in Task-1

--- scripts/Task-1/p1.sh
+++ scripts/Task-1/p1.sh
@@ -3,11 +3,11 @@
 name="JOHN DOE";
 echo "Hello, $name";
 
-output1=`expr 5 + 6`;
+output1=`expr 5 + 121`;
 output2=$(( 5 + 11 ));
 
-if [ "$output1" -ge "$output2" ]; then
-    echo "$output1 is greater than or equal $output2.";
+if [ "$output1" -ge "$output2" ] && [ "$output1" -gt 10 ]; then
+    echo "$output1 is greater than or equal $output2 and also greater than 10.";
 elif [ "$output1" -lt "$output2" ]; then
     echo "$output1 is less than $output2.";
 fi
